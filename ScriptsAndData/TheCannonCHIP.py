061feef06da943b545764273bbd13cc3ad6cd835
jgussman <jgussman@iu.edu>
2022-03-21 12:53:46 -0400

    Made function for reusable code

--- ScriptsAndData/TheCannonCHIP.py
+++ ScriptsAndData/TheCannonCHIP.py
@@ -118,48 +118,58 @@ def TheCannonCHIP(wavelength_file_path,fluxes_file_path,ivar_file_path,id_file_p
         return groups_list
             
 
+    def ApplyMask(file_path,flux_tr,flux_te,ivar_tr,ivar_te):
+        '''
+        Applies mask if there is a given file path
+        '''
+        if mask_file_path:
+            mask = np.load(mask_file_path)
+            u_flux_tr = np.copy(flux_tr) * mask
+            u_flux_te  = np.copy(flux_te) * mask 
+            u_ivar_tr = np.copy(ivar_tr) * mask
+            u_ivar_te  = np.copy(ivar_te) * mask
+        else: 
+            u_flux_tr = np.copy(flux_tr)
+            u_flux_te  = np.copy(flux_te)
+            u_ivar_tr = np.copy(ivar_tr)
+            u_ivar_te  = np.copy(ivar_te)
+        return u_flux_tr, u_flux_te, u_ivar_tr, u_ivar_te
+
+    num_stars = len(ids)
+    def IntialParamRecording(s,t_p,v_p,m_n,s_n,l_m_n):
+        g_results = {"# Stars":num_stars, 
+                                "Test %" :t_p *100, 
+                                "Valid %": v_p *100, 
+                                "Seed":random_seed,
+                                "Group Size": s,
+                                "Mask": m_n,
+                                "Scale Abun":s_n,
+                                "Loss-Metric":l_m_n}
+        # Add your labels to the results
+        for label in parameters:
+            g_results[label] = float('-inf')
+        return g_results
 
 
 
+
+    # Create Df for storing Results
+    results_df = pd.DataFrame(columns=["# Stars","Test %","Valid %","Seed","Group Size","Mask","Scale Abun","Loss-Metric"] + [label for label in parameters])
     num_training_parameters = len(parameters) 
-    results_df = pd.DataFrame(columns=["# Stars","test %","Valid %","Seed","Group Size","Mask","Scale Abun","Loss-Metric"] + [label for label in parameters])
-
-
 
     for mask_info in masks_list:
         mask_name, mask_file_path = mask_info
-        if mask_file_path:
-            mask = np.load(mask_file_path)
-            u_flux_train = flux_train * mask
-            u_flux_valid = flux_valid * mask 
-            u_ivar_train = ivar_train * mask
-            u_ivar_valid = ivar_valid * mask 
-        else: 
-            u_flux_train = flux_train 
-            u_flux_valid = flux_valid 
-            u_ivar_train = ivar_train 
-            u_ivar_valid = ivar_valid 
+        u_flux_train,u_flux_valid,u_ivar_train,u_ivar_valid = ApplyMask(mask_file_path,flux_train,flux_test,ivar_train,ivar_test) 
 
         for scaler_info in abundance_scaler_list:
             scaler_name,scaler_instance = scaler_info 
-        # Must apply scale later since the shape needs to have the same columns 
+            # Must apply scale later since the shape needs to have the same columns 
             
 
             for size in group_sizes_list:
                 training_groups = TrainingBuddys(parameters,size)
-
-                #Format for Results
-                group_results = {"# Stars":len(ids), 
-                                "test %" :testing_percentage *100, 
-                                "Valid %": validation_percentage *100, 
-                                "Seed":random_seed,
-                                "Group Size": size,
-                                "Mask": mask_name,
-                                "Scale Abun":scaler_name,
-                                "Loss-Metric":loss_metric_name}
-                # Add your labels to the results
-                for label in parameters:
-                    group_results[label] = float('-inf')
+                group_results = IntialParamRecording(size,testing_percentage,validation_percentage,mask_name,scaler_name,loss_metric_name)
+                
 
                 for i in range(len(training_groups)):
                     group_parameters = training_groups[i]
@@ -193,7 +203,9 @@ def TheCannonCHIP(wavelength_file_path,fluxes_file_path,ivar_file_path,id_file_p
     
 
                             group_results[parameters[j+y]] = loss_metric_fun(abun_valid[:,j+y],u_infered_valid_labels[:,y])
+
                 results_df = results_df.append([group_results])
+
 
 
     #Reapply Train Test Split 
@@ -206,7 +218,7 @@ def TheCannonCHIP(wavelength_file_path,fluxes_file_path,ivar_file_path,id_file_p
                     random_state=random_seed) 
 
 
-
+    # Find Best Model
     best_model = results_df.iloc[0]
     num_of_half_parameters = len(parameters)//2
     for row in range(1,results_df.shape[0]):
@@ -228,34 +240,13 @@ def TheCannonCHIP(wavelength_file_path,fluxes_file_path,ivar_file_path,id_file_p
     {best_model}
     ''')
 
-    #Format for Results
-    group_results = {"# Stars":len(ids), 
-                    "test %" :best_model["test %"], 
-                    "Valid %": 0, 
-                    "Seed":best_model["Seed"],
-                    "Group Size": best_model["Group Size"],
-                    "Mask": best_model["Mask"],
-                    "Scale Abun":best_model["Scale Abun"],
-                    "Loss-Metric":best_model["Loss-Metric"]}
-    # Add your labels to the results
-    for label in parameters:
-        group_results[label] = float('-inf')
+    group_results = IntialParamRecording(best_model["Group Size"],best_model["Test %"],0, best_model["Mask"],best_model["Scale Abun"],best_model["Loss-Metric"])
     
-
+    
     ## Apply Best Mask
     best_mask_name = best_model["Mask"]
-    mask_file_path = [mask[1] for mask in masks_list if mask[0] == best_mask_name][0]
-    if mask_file_path:
-        mask = np.load(mask_file_path)
-        u_flux_train = flux_train * mask
-        u_flux_test  = flux_test * mask 
-        u_ivar_train = ivar_train * mask
-        u_ivar_test  = ivar_test * mask
-    else: 
-        u_flux_train = flux_train 
-        u_flux_test  = flux_test 
-        u_ivar_train = ivar_train 
-        u_ivar_test  = ivar_test
+    mask_file_path = [mask[1] for mask in masks_list if mask[0] == best_mask_name][0] # Find the mask tuple
+    u_flux_train,u_flux_test,u_ivar_train,u_ivar_test = ApplyMask(mask_file_path,flux_train,flux_test,ivar_train,ivar_test)
 
     ## Apply Best Scaler
     best_scaler_name = best_model["Scale Abun"]
@@ -293,26 +284,22 @@ def TheCannonCHIP(wavelength_file_path,fluxes_file_path,ivar_file_path,id_file_p
 
         # Infer labels & useful plots
         label_errs = md.infer_labels(ds)
-        md.diagnostics_leading_coeffs(ds)
-        md.diagnostics_plot_chisq(ds)
+        # md.diagnostics_leading_coeffs(ds)
+        # md.diagnostics_plot_chisq(ds)
         infered_test_labels = ds.test_label_vals
 
         #Calculate score and store
         for y in range(size):
             if j+y < num_training_parameters:
-                u_abun_test = abun_test
                 if scaler_instance:
                     u_infered_test_labels = scaler_instance.inverse_transform(infered_test_labels)
                 else:
                     u_infered_test_labels = infered_test_labels
 
-                group_results[parameters[j+y]] = loss_metric_fun(u_abun_test[:,j+y],u_infered_test_labels[:,y])
-                SaveTrueAndPredicted(u_abun_test[:,j+y],u_infered_test_labels[:,y],parameters[j+y])
+                group_results[parameters[j+y]] = loss_metric_fun(abun_test[:,j+y],u_infered_test_labels[:,y])
+                SaveTrueAndPredicted(abun_test[:,j+y],u_infered_test_labels[:,y],parameters[j+y])
         results_df = results_df.append([group_results])
         
-
-
-
     results_df.to_csv("Results",index=False)
     print("The very last model added to Results.csv is the model trained with the best hyperparameters and whole training set (including the validation set)")
     print("Run Plot_Results.py to graph the results")
