54dfede0beaad714a619a405f693c7831f5fa4cb
jgussman <jgussman@iu.edu>
2022-11-25 12:35:45 -0600

    download_spectra working normally

--- src/CHIP2.py
+++ src/CHIP2.py
@@ -1,9 +1,14 @@
 from astropy.io import fits
 from hiresprv.auth import login
+from hiresprv.database import Database
+from hiresprv.download import Download
+from hiresprv.idldriver import Idldriver
+
 
 import logging
 import json
 import os
+import numpy as np
 import pandas as pd
 import time 
 
@@ -23,6 +28,14 @@ class CHIP:
 
         # Create a new storage location for this pipeline
         self.create_storage_location()
+
+        # HIRES Filename : spectrum as a (16,4021) np.array  
+        self.spectraDic = {}            
+
+        # For storing sigma valeus
+        self.ivarDic = {}                
+
+                                                                                     
     
     
     def create_storage_location(self):
@@ -32,14 +45,15 @@ class CHIP:
 
         Outputs: None 
         '''
-        logging.info("create_storage_location()")
+        logging.debug("create_storage_location()")
 
         # Greenwich Mean Time (UTC) right now 
-        gmt_datetime = time.strftime("%Y-%m-%d_%H-%M",time.gmtime())
+        gmt_datetime = time.strftime("%Y-%m-%d_%H-%M-%S",time.gmtime())
 
         self.storage_path = os.path.join("data","chip_runs" , gmt_datetime )
 
         os.makedirs(self.storage_path,exist_ok=True)
+        
         
 
     def run(self):
@@ -54,7 +68,6 @@ class CHIP:
 
         pass 
 
-
     def get_arguments(self):
         ''' Get arguments from src/config.json 
 
@@ -63,101 +76,52 @@ class CHIP:
         Outputs: (dict) representing src/config.json
         '''
 
-        logging.info("get_arguments()")
+        logging.debug("get_arguments()")
 
         with open("src/config.json", "r") as f:
             self.config = json.load(f)
         
         logging.info( f"config.json : {self.config}" )
+    
+    @staticmethod
+    def calculate_SNR(spectrum):
+        ''' Calculates the SNR of spectrum using the 8th and 9th echelle orders
 
-        
-    def download_spectra(self):
+        Inputs: spectrum (np.array) - spectrum with at least 9 rows representing echelle orders 
+
+        Outputs: (float) estimated SNR of inputed spectrum
         '''
+        logging.debug(f"calculate_SNR({spectrum})")
 
-        Inputs: None
+        spectrum = spectrum[7:10].flatten() # Using echelle orders in the middle 
+        SNR = np.mean(np.sqrt(spectrum))
+        return SNR 
 
-        Outputs: None
+    def delete_spectrum(self,filename):
+        '''Remove spectrum from local storage
+
+        Inputs: filename (str): HIRES file name of spectrum you want to delete
+
+        Outputs: None 
         '''
-
-        # Cross matched names 
-        cross_matched_file_path = self.config["CHIP"]["cross_match_stars"]["val"]
-        cross_matched_df = pd.read_csv( cross_matched_file_path, sep=" " )
-        hires_names_array = cross_matched_df["HIRES"].to_numpy()
-
-        removed_stars = {"no RV observations":[],"rvcurve wasn't created":[], "SNR < 100":[],"NAN value in spectra":[],"No Clue":[]}
-        hiresName_fileName_snr_dic = {"HIRESName": [],"FILENAME":[],"SNR":[]}  
-
-        
-        for star_ID in hires_names_array:
-            logging.info(f"Finding highest SNR spectrum for {star_ID}")
-                
-            try:
-                # SQL query to find all the RV observations of one particular star
-                search_string = f"select OBTYPE,FILENAME from FILES where TARGET like '{star_ID}' and OBTYPE like 'RV observation';"
-                url = self.state.search(sql=search_string)
-                obs_df = pd.read_html(url, header=0)[0]
-                # There are no RV Observations
-                if obs_df.empty: 
-                    logging.info(f"{star_ID} has no RV observations")
-                    removed_stars["no RV observations"].append(star_ID) 
-                    continue 
-                else:   
-
-                    best_SNR = 0
-                    best_SNR_filename = False 
-                    for filename in obs_df["FILENAME"]:
-                        temp_SNR = self.DownloadSpectrum(filename)
-                        
-                        # Check if the SNR is the highest out of 
-                        # all the star's previous spectras 
-                        if best_SNR < temp_SNR:
-                            best_SNR = temp_SNR
-
-                            best_SNR_filename = filename 
-
-                    if best_SNR < 100: 
-                        logging.info(f"{star_ID}'s best spectrum had an SNR lower than 100. Thus it was removed.")
-
-                        removed_stars["SNR < 100"].append(star_ID)
-
-                    else:
-                        # Save the Best Spectrum
-                        self.spectraDic[best_SNR_filename] = self.DownloadSpectrum(best_SNR_filename, 
-                                                                                SNR=False)
-                        
-
-                        hiresName_fileName_snr_dic["HIRESName"].append(star_ID)
-                        hiresName_fileName_snr_dic["FILENAME"].append(best_SNR_filename)
-                        hiresName_fileName_snr_dic["SNR"].append(best_SNR)
-                    
-                        # Calculate ivar
-                        self.SigmaCalculation(best_SNR_filename)
-
-            except:
-                logging.info(f"{star_ID} was removed for an undefined reason.")
-                removed_stars["No Clue"].append(star_ID)
-            finally:
-                continue
-
-        self.filename_df = pd.DataFrame(hiresName_fileName_snr_dic) 
-
-        os.mkdir()
-        self.filename_df.to_csv("HIRES_Filename_snr.csv",index_label=False,index=False)
-        
-        print(removed_stars)
-        print("Downloading Spectra Has Finished")
-        
+        logging.debug(f"delete_spectrum({filename})")
+        file_path = os.path.join(self.dataSpectra.localdir,filename + ".fits")
+        try:
+            os.remove(file_path)
+        except FileNotFoundError as e:
+            # file was already deleted 
+            pass 
 
 
-        def download_spectrum(self,filename,SNR = True):
+    def download_spectrum(self,filename,SNR = True):
             '''Download Individual Spectrum and ivar 
 
-            Input: filename (str): name of spectrum you want to download
+            Input: filename (str): HIRES file name of spectrum you want to download
                    SNR (bool): if you want to calculate the SNR of the spectrum 
 
             Output: None
             '''
-            logging.info(f"download_spectrum(filename={filename},SNR={SNR})")
+            logging.debug(f"download_spectrum(filename={filename},SNR={SNR})")
             
             #Download spectra
             self.dataSpectra.spectrum(filename.replace("r",""))    
@@ -171,16 +135,141 @@ class CHIP:
             
             
             if SNR: # Used to find best SNR 
-                # Delete Spectrum to save space
                 SNR = self.calculate_SNR(temp_deblazedFlux)
+
+                # delete Spectrum variable so it can be delete if needed
                 del temp_deblazedFlux 
-                self.DeleteSpectrum(filename)
                 return SNR
 
             else:
                 return temp_deblazedFlux
         
         
+    def download_spectra(self):
+        ''' Downloads all the spectra for each star in the NExSci, calculates the SNR and saves the 
+        spectrum with the highest SNR for each star.
+
+        Inputs: None
+
+        Outputs: None
+        '''
+
+        # Cross matched names 
+        cross_matched_file_path = self.config["CHIP"]["cross_match_stars"]["val"]
+        cross_matched_df = pd.read_csv( cross_matched_file_path, sep=" " )
+        hires_names_array = cross_matched_df["HIRES"].to_numpy()
+
+        # Record results 
+        removed_stars = {"no RV observations":[],"rvcurve wasn't created":[], "SNR < 100":[],"NAN value in spectra":[],"No Clue":[]}
+        hiresID_fileName_snr_dic = {"HIRESid": [],"FILENAME":[],"SNR":[]}  
+
+        # Location to save spectra
+        spectra_download_location = os.path.join( self.storage_path, "rv_obs" )
+        os.makedirs( spectra_download_location, exist_ok=True )
+        # For retrieving data from HIRES
+        self.state = Database('data/prv.cookies')
+        self.dataSpectra = Download('data/prv.cookies', spectra_download_location)
+                                  
+        
+        for star_ID in hires_names_array:
+            logging.info(f"Finding highest SNR spectrum for {star_ID}")
+                
+            
+            # SQL query to find all the RV observations of one particular star
+            search_string = f"select OBTYPE,FILENAME from FILES where TARGET like '{star_ID}' and OBTYPE like 'RV observation';"
+            url = self.state.search(sql=search_string)
+            obs_df = pd.read_html(url, header=0)[0]
+
+            # Check if there are any RV Observations 
+            if obs_df.empty: 
+                logging.info(f"{star_ID} has no RV observations")
+                removed_stars["no RV observations"].append(star_ID) 
+                continue 
+            else:   
+                logging.debug(f"RV Observation Filenames: {obs_df['FILENAME'] }")
+
+                best_SNR = 0
+                best_SNR_filename = False 
+
+                for filename in obs_df["FILENAME"]:
+                    temp_SNR = self.download_spectrum(filename)
+                    
+
+                    # Check if the SNR is the highest out of 
+                    # all the star's previous spectras 
+                    if best_SNR < temp_SNR:
+                        # Since this spectrum is not longer the best, we will delete it
+                        delete_spectrum_filename = best_SNR_filename 
+
+                        best_SNR = temp_SNR
+                        best_SNR_filename = filename 
+                    else:
+                        delete_spectrum_filename = filename 
+
+                    # Delete unused spectrum
+                    if delete_spectrum_filename: # Will not trigger in the intial case
+                        self.delete_spectrum(delete_spectrum_filename)
+
+
+                if best_SNR < 100: 
+                    logging.info(f"{star_ID}'s best spectrum had an SNR lower than 100. Thus it was removed.")
+
+                    removed_stars["SNR < 100"].append(star_ID)
+
+                else:
+                    # Save the Best Spectrum
+                    self.spectraDic[best_SNR_filename] = self.download_spectrum(best_SNR_filename, 
+                                                                                SNR=False)
+                    
+                    logging.info(f"{star_ID}'s best SNR spectrum came from {best_SNR_filename} with an SNR={best_SNR}")
+                    hiresID_fileName_snr_dic["HIRESid"].append(star_ID)
+                    hiresID_fileName_snr_dic["FILENAME"].append(best_SNR_filename)
+                    hiresID_fileName_snr_dic["SNR"].append(best_SNR)
+                
+                    # Calculate ivar
+                    self.sigma_calculation(best_SNR_filename)
+
+            
+        
+        # Save SNR meta data in csv file 
+        self.hires_filename_snr_df = pd.DataFrame(hiresID_fileName_snr_dic) 
+        self.hires_filename_snr_df.to_csv( os.path.join(self.storage_path ,"HIRES_Filename_snr.csv"),
+                                           index_label=False,
+                                           index=False)
+        
+        pd.DataFrame(removed_stars).to_csv( os.path.join(self.storage_path ,"remove_stars.csv"),
+                                           index_label=False,
+                                           index=False) 
+
+        # Delete unused instance attributes
+        del self.dataSpectra
+        del self.state
+        
+
+    def sigma_calculation(self,filename):
+        '''Calculates sigma for inverse variance (IVAR) 
+
+        Inputs: filename (str): HIRES file name of spectrum you want to calculate IVAR for
+
+        Outputs: None
+        '''
+        gain = 1.2 #electrons/ADU
+        readn = 2.0 #electrons RMS
+        xwid = 5.0 #pixels, extraction width
+
+        sigma = np.sqrt((gain*self.spectraDic[filename]) + (xwid*readn**2))/gain 
+        #Checkinng for a division by zeros 
+        if not np.isnan(sigma).any(): #Happens in the IVAR
+                self.ivarDic[filename] = sigma
+        else:
+            logging.error(f"****{filename} HAS BEEN REMOVED BECAUSE IT CONTAINS NAN VALUES IN IVAR") 
+            del self.spectraDic[filename]
+
+
+        
+        
+
+
 
 
 
@@ -192,18 +281,22 @@ if __name__ == "__main__":
     # logging 
     logging.basicConfig(filename='src/CHIP.log',
                         format='%(asctime)s - %(message)s', 
-                        datefmt='%m/%d/%Y %I:%M:%S %p',  
+                        datefmt="%Y/%m/%d %H:%M:%S",  
                         level=logging.INFO)
     # logs to file and stdout
     logging.getLogger().addHandler(logging.StreamHandler())
+    # set datefmt to GMT
+    logging.Formatter.converter = time.gmtime
 
     logging.info(f"Running file: {__file__}")
 
     # logging into the NExSci servers
-    login('prv.cookies')
+    login('data/prv.cookies')
 
     # Instantiation
     chip = CHIP()
+
+    chip.download_spectra()
 
     
 
